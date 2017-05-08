fb13a1e894730fb28fe5fb703404e861537bbe2b
Blake Womack and Gloria Friesen <gloria@>
2017-05-08 16:24:35 -0700

    add listing service

--- src/app/listings/listings.component.ts
+++ src/app/listings/listings.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Listing } from '../listing.model';
 import { Router } from '@angular/router';
+import { ListingService } from '../listing.service';
 
 @Component({
   selector: 'app-listings',
   templateUrl: './listings.component.html',
-  styleUrls: ['./listings.component.css']
+  styleUrls: ['./listings.component.css'],
+  providers: [ListingService]
 })
 export class ListingsComponent implements OnInit {
-  listings: Listing[] = [
-    new Listing("Tent", 45, "camping", "tent.jpg", "Blue tent. No holes. Used once.", "Blake", 1),
-    new Listing("Roller blades", 15, "skates", "skates.jpg", "Pristine. Never been worn.Didn't like 'em, didn't use 'em.'", "Gloria", 2),
-    new Listing ("Kayak", 300, "water sports", "kayak.jpg", "Couple of bumps and bruises, but still keeps out the water. No holes.", "Grace", 3)
-  ];
+  listings: Listing[] = [];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private listingService: ListingService) { }
 
   ngOnInit() {
+    this.listings = this.listingService.getListings();
   }
 
   goToDetailPage(clickedListing: Listing) {
---
+++ src/app/mock-listings.ts
@@ -0,0 +1,7 @@
+import { Listing } from './listing.model';
+
+export const LISTINGS: Listing[] = [
+  new Listing("Tent", 45, "camping", "tent.jpg", "Blue tent. No holes. Used once.", "Blake", 1),
+  new Listing("Roller blades", 15, "skates", "skates.jpg", "Pristine. Never been worn.Didn't like 'em, didn't use 'em.'", "Gloria", 2),
+  new Listing ("Kayak", 300, "water sports", "kayak.jpg", "Couple of bumps and bruises, but still keeps out the water. No holes.", "Grace", 3)
+];
