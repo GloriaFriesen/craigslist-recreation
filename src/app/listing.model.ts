deb13cb4a0fcbb287242871d7dd4fc1cba9b46d9
Blake Womack and Gloria Friesen <blake@>
2017-05-08 15:45:35 -0700

    add array of listings to listings component to display items for sale

---
+++ src/app/listing.model.ts
@@ -0,0 +1,3 @@
+export class Listing {
+  constructor (public item: string, public price: number, public category: string, public image: string, public description: string, public seller: string, public id: number) { }
+}
--- src/app/listings/listings.component.ts
+++ src/app/listings/listings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../listing.model';
 
 @Component({
   selector: 'app-listings',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listings.component.css']
 })
 export class ListingsComponent implements OnInit {
+  listings: Listing[] = [
+    new Listing("Tent", 45, "camping", "tent.jpg", "Blue tent. No holes. Used once.", "Blake", 1),
+    new Listing("Roller blades", 15, "skates", "skates.jpg", "Pristine. Never been worn.Didn't like 'em, didn't use 'em.'", "Gloria", 2),
+    new Listing ("Kayak", 300, "water sports", "kayak.jpg", "Couple of bumps and bruises, but still keeps out the water. No holes.", "Grace", 3)
+  ];
 
   constructor() { }
 
